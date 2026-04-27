65ddfe69d2b9dc30f07a03e951fd28651ae4385d
AY-AllenYe <919874668@qq.com>
2026-04-27 22:07:58 +0800

    accepted

--- LeetCode/Hot100/283 Move Zero/283 Move Zero.cpp
+++ LeetCode/Hot100/283 Move Zero/283 Move Zero.cpp
@@ -7,31 +7,26 @@ using namespace std;
 
 class Solution {
 public:
-    int longestConsecutive(vector<int>& nums) {
-        unordered_set<int> new_set;
-        for (int num : nums) {
-            new_set.insert(num);
-        }
-        int longest = 0;
-        for (int num : new_set) {
-            if (!new_set.count(num - 1)) {
-                int currentNum = num;
-                int currentStreak = 1;
-                while (new_set.count(currentNum + 1)) {
-                    currentNum++;
-                    currentStreak++;
+    vector<int> moveZeroes(vector<int>& nums) {
+        int left = 0, right = 0;
+        int tmp;
+        while (right < nums.size()) {
+            if (nums[right]) {
+                if(left != right){
+                    nums[left] = nums[right];
+                    nums[right] = 0;
                 }
-                longest = max(longest, currentStreak);
+                left++;
             }
+            right++;
         }
-        return longest;
+        return nums;
     }
 };
-        
 
 int main() {
     Solution solution;
-    vector<string> strs;
+    vector<int> result;
     
     int n;
     vector<int> num;
@@ -42,10 +37,15 @@ int main() {
         num.push_back(value);
     }
 
-    auto result = solution.longestConsecutive(num);
+    result = solution.moveZeroes(num);
     
     // 输出结果
-    cout << result;
+    for(int i = 0; i < result.size(); i++){
+        cout << result[i];
+        if(i != result.size() - 1){
+            cout  << ",";
+        }
+    }
     
     return 0;
 }
